e4dbbb81f2f4ebc25f5a4ee13624f434c93ccdcb
Utsob1621 <utsobsaha9072@gmail.com>
2026-02-11 01:11:29 +0600

    images src made dynamic

--- script/script.js
+++ script/script.js
@@ -10,20 +10,24 @@
 
 const ringButtons = document.querySelectorAll(".ring-button");
 
-for (let i = 0; i < ringButtons.length;  i++) {
+for (let i = 0; i < ringButtons.length; i++) {
   const ringBtn = ringButtons[i];
 
   ringBtn.addEventListener("click", function (event) {
-    for (let j = 0; j < ringButtons.length; j++){
+
+    const color = event.target.id.replace("-color", "");
+
+    for (let j = 0; j < ringButtons.length; j++) {
       ringButtons[j].classList.remove("border-purple-600");
       ringButtons[j].classList.add("border-gray-300");
     }
-    
+
     // color add
     event.target.classList.add("border-purple-600");
     event.target.classList.remove("border-gray-600");
 
     const productImage = document.getElementById("product-image");
-    productImage.src = "../assets/gray.png"
+    // productImage.src = "../assets/gray.png"
+    productImage.src = "../assets/" + color + ".png";
   });
 } 
